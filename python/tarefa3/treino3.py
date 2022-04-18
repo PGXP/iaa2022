ed6d19091716b2b955411fadd63ccf86314ce065
PP Casa <paulopinheiro777@gmail.com>
2022-04-18 20:42:56 -0300

    foi

--- python/tarefa3/treino3.py
+++ python/tarefa3/treino3.py
@@ -8,7 +8,7 @@ Created on Sat Apr  9 10:16:26 2022
 
 from sklearn.model_selection import train_test_split
 import pandas as pd
-data = pd.read_json('/home/desktop/Documentos/bibliaTeste.json')
+data = pd.read_json('/home/desktop/Documentos/bibliaV0L1.json')
 
 import nltk
 nltk.download('stopwords')
@@ -27,37 +27,43 @@ from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(dataSSW)
 
-from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB().fit(X_train_counts, data["livro"])
-
 from sklearn.feature_extraction.text import TfidfTransformer
-tfidf_transformer = TfidfTransformer()
+tfidf_transformer = TfidfTransformer(use_idf=(False))
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-docs_new = ['Sê bendito, Senhor, para sempre', 'Pelos frutos das nossas jornadas!']
-X_new_counts = count_vect.transform(docs_new)
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB().fit(X_train_tfidf, data["capitulo"])
+
+docs_new = ['Sê bendito, Senhor, para sempre', 'Pelos frutos das nossas jornadas']
+dataQuest = []
+
+for frase in docs_new:
+    words = ' '.join([palavra for palavra in frase.split() if not palavra.lower() in stop_words])
+    dataQuest.append(words)
+    
+X_new_counts = count_vect.transform(dataQuest)
 X_new_tfidf = tfidf_transformer.transform(X_new_counts)
 
 predicted = clf.predict(X_new_tfidf)
 
-for doc, category in zip(docs_new, predicted):
-    print('%r => %s' % (doc, data["livro"]))
+for doc, category in zip(dataQuest, predicted):
+    print('%r => %s' % (doc, data["capitulo"]))
     
     
     
 from sklearn import metrics
-print(metrics.classification_report(data["livro"], predicted,
-                                    data["livro"]))
+print(metrics.classification_report(X_train_tfidf, predicted,
+                                    dataQuest))
                                             
 metrics.confusion_matrix(data["livro"], predicted)
     
 from sklearn.pipeline import Pipeline
 text_clf = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer()),
+                     ('tfidf', TfidfTransformer(use_idf=False)),
                      ('clf', MultinomialNB()),
 ])
     
-text_clf.fit(X_test, y_test) 
+text_clf.fit(X_new_tfidf, dataQuest) 
 
 import numpy as np
 docs_test = y_train
@@ -67,15 +73,15 @@ np.mean(predicted == y_test)
 
 from sklearn.linear_model import SGDClassifier
 text_clf = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer()),
+                     ('tfidf', TfidfTransformer(use_idf=False)),
                      ('clf', SGDClassifier(loss='hinge', penalty='l2',
                                            alpha=1e-3, random_state=42,
                                            max_iter=5, tol=None)),
 ])
-text_clf.fit(data["texto"], data["livro"])  
+text_clf.fit(X_new_tfidf, dataQuest)  
 
-predicted = text_clf.predict(docs_test)
-np.mean(predicted == twenty_test.target)
+predicted = text_clf.predict(dataQuest)
+np.mean(predicted == data["capitulo"])
 
 
 
