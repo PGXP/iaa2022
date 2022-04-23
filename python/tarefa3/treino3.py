a88f6da72caa09adabdc1a1e8c03cd31cad6da78
PP Casa <paulopinheiro777@gmail.com>
2022-04-23 18:04:26 -0300

    foi

--- python/tarefa3/treino3.py
+++ python/tarefa3/treino3.py
@@ -7,68 +7,59 @@ Created on Sat Apr  9 10:16:26 2022
 """
 
 from sklearn.model_selection import train_test_split
-import pandas as pd
-data = pd.read_json('/home/desktop/Documentos/bibliaV0L1.json')
-
 import nltk
+from nltk.tokenize import RegexpTokenizer
 nltk.download('stopwords')
 stop_words = nltk.corpus.stopwords.words('portuguese')
+tokenizer = RegexpTokenizer(r'[A-z]\w*')
+
+import pandas as pd
+data = pd.read_json('/home/desktop/Documentos/biblia.json')
 
 dataSSW = []
 
-for frase in data["texto"]:
-    words = ' '.join([palavra for palavra in frase.split() if not palavra.lower() in stop_words])
+for letra in data["texto"]:
+    tokens = tokenizer.tokenize(letra)
+    tokens_without_sw = [frase for frase in tokens if not str(frase).lower() in stop_words]
+    words = " ".join(str(x).lower() for x in tokens_without_sw)
     dataSSW.append(words)
 
-#X_train, X_test, y_train, y_test = train_test_split(dataSSW, data["livro"], test_size=0.2, random_state=7)
-
+#X_train, X_test, y_train, y_test = train_test_split(dataSSW, data["livro"],test_size=0.2, random_state=66)
 
 from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(dataSSW)
 
 from sklearn.feature_extraction.text import TfidfTransformer
-tfidf_transformer = TfidfTransformer(use_idf=(False))
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+tf_transformer = TfidfTransformer(use_idf=True).fit(X_train_counts)
+X_train_tf = tf_transformer.transform(X_train_counts)
 
 from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB().fit(X_train_tfidf, data["capitulo"])
+clf = MultinomialNB().fit(X_train_tf, data["livro"])
 
-docs_new = ['Sê bendito, Senhor, para sempre', 'Pelos frutos das nossas jornadas']
-dataQuest = []
+docs_new = ['O meu combustível pra continuar', 'Jesus é a calmaria, o aconchego dos meus dias',
+            'O meu alicerce pra não desistir', 'Não tá sendo fácil aqui, mas eu tenho que seguir',
+            'Tem dias que o peito aperta', 'Tem dias que o fardo pesa',
+            'Nem que for se arrastando']
+X_new_counts = count_vect.transform(docs_new)
+# X_new_tfidf = tfidf_transformer.transform(X_new_counts)
 
-for frase in docs_new:
-    words = ' '.join([palavra for palavra in frase.split() if not palavra.lower() in stop_words])
-    dataQuest.append(words)
-    
-X_new_counts = count_vect.transform(dataQuest)
-X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+predicted = clf.predict(X_new_counts)
 
-predicted = clf.predict(X_new_tfidf)
-
-for doc, category in zip(dataQuest, predicted):
-    print('%r => %s' % (doc, data["capitulo"]))
-    
-    
-    
-from sklearn import metrics
-print(metrics.classification_report(X_train_tfidf, predicted,
-                                    dataQuest))
-                                            
-metrics.confusion_matrix(data["livro"], predicted)
+for doc, category in zip(docs_new, predicted):
+    print('%r => %s' % (doc, category))
     
 from sklearn.pipeline import Pipeline
 text_clf = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer(use_idf=False)),
+                     ('tfidf', TfidfTransformer()),
                      ('clf', MultinomialNB()),
 ])
     
-text_clf.fit(X_new_tfidf, dataQuest) 
+text_clf.fit(dataSSW, data["livro"]) 
 
 import numpy as np
-docs_test = y_train
-predicted = text_clf.predict(docs_test)
-np.mean(predicted == y_test)
+predicted = text_clf.predict(X_new_counts)
+np.mean(predicted == data["livro"])
 
 
 from sklearn.linear_model import SGDClassifier
@@ -78,12 +69,15 @@ text_clf = Pipeline([('vect', CountVectorizer()),
                                            alpha=1e-3, random_state=42,
                                            max_iter=5, tol=None)),
 ])
-text_clf.fit(X_new_tfidf, dataQuest)  
+text_clf.fit(dataSSW, data["livro"])  
 
-predicted = text_clf.predict(dataQuest)
-np.mean(predicted == data["capitulo"])
+predicted = text_clf.predict(X_new_counts)
+np.mean(predicted == data["livro"])
 
-
+from sklearn import metrics
+print(metrics.classification_report(data["livro"], predicted))
+                                        
+metrics.confusion_matrix(data["livro"], predicted)
 
 from sklearn.model_selection import GridSearchCV
 parameters = {'vect__ngram_range': [(1, 1), (1, 2)],
@@ -92,8 +86,8 @@ parameters = {'vect__ngram_range': [(1, 1), (1, 2)],
 }
 
 gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
-gs_clf = gs_clf.fit(twenty_train.data[:400], twenty_train.target[:400])
-twenty_train.target_names[gs_clf.predict(['God is love'])[0]]
+gs_clf = gs_clf.fit(dataSSW, data["livro"])
+gs_clf.predict(['Deus é amor'])
 
 gs_clf.best_score_                                  
 
