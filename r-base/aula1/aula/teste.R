131cd1d890d667016b3fcf017f8bf894fed4e2e0
PP Notebook <paulopinheiro777@gmail.com>
2022-05-07 14:52:24 -0300

    Foi

--- r-base/aula1/aula/teste.R
+++ r-base/aula1/aula/teste.R
@@ -1,11 +1,47 @@
 produtos <- read.csv("~/Documentos/produtos.csv", sep=";")
 
+--------------
+install.packages("plyr")
+library(plyr)
 View(produtos)
+order(count(produtos, 'produto')$freq)
+--------------
+  
+produtos210 <- produtos[which(produtos$produto == 210),names(produtos) %in% c("dia","valor")]
 
-tabela = aggregate(x = produtos$valor, by = list(produtos$dia), FUN = mean)
+hist(produtos210$dia)
+
+proDia <- subset(produtos210, select = c("dia","valor"))
+proDia$dia <- as.factor(proDia$dia)
+
+str(proDia)
+
+boxplot(proDia$valor~proDia$dia, xlab = 'dia', ylab='valor', main='Tabela',data = proDia)
+
+summary(proDia)
+
+linha = lm(proDia$valor ~ proDia$dia, data=proDia)
+
+plot(linha)
+
+View(proDia)
+
+hist(proDia)
+
+tabelaDia = aggregate(x = produtos$valor, by = list(produtos$dia), FUN = mean)
+
+
+
+boxplot(tabelaDia$x~tabelaDia$Group.1, xlab = 'dia', ylab='valor', main='Tabela',data = tabelaDia)
+plot(tabelaDia$x~tabelaDia$Group.1, xlab = 'dia', ylab='valor', main='Tabela',data = tabelaDia)
+
+tabelaBairro = aggregate(x = produtos$valor, by = list(produtos$bairro), FUN = mean)
+
+boxplot(tabelaBairro$x~tabelaBairro$Group.1, xlab = 'dia', ylab='valor', main='Tabela',data = tabelaBairro)
+
 print(tabela)
 plot(tabela)
 
-linha = lm(tabela$Group.1~tabela$x, data=tabela)
+linha = lm(tabelaDia$Group.1 ~tabelaDia$x, data=tabelaDia)
 
-plot(linha)
+boxplot(linha)
